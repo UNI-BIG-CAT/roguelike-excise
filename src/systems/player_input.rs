2313878eb4c2387030cb6fcc4b4f66d1861a6b14
fengyujia <549515264@qq.com>
2025-09-01 11:34:58 +0800

    回血

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -21,6 +21,8 @@ use legion::world::SubWorld;
 #[system]
 #[write_component(Point)] // 表示这个系统需要写入 Point 组件 允许系统修改实体的位置
 #[read_component(Player)] // 表示这个系统需要读取 Player 组件 允许系统识别哪些实体是玩家
+#[read_component(Enemy)] // 表示这个系统需要读取 Enemy 组件 允许系统识别哪些实体是敌人
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld, // 自动注入 ECS 世界
     commands: &mut CommandBuffer,
@@ -44,11 +46,13 @@ pub fn player_input(
         if delta.x != 0 || delta.y != 0 {
             let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
             let mut hit_something = false;
+            let mut did_something = false;
             enemies
                 .iter(ecs)
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
                     commands.push((
                         (),
                         WantsToAttack {
@@ -58,6 +62,7 @@ pub fn player_input(
                     ));
                 });
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -66,6 +71,16 @@ pub fn player_input(
                     },
                 ));
             }
+            if did_something {
+                if let Ok(health) = ecs
+                    .entry_mut(player_entity)
+                    .unwrap()
+                    .get_component_mut::<Health>()
+                {
+                    health.current = i32::min(health.max, health.current + 1);
+                }
+            }
+
             *turn_state = TurnState::PlayerTurn;
         }
         // players.iter(ecs).for_each(|(entity, pos)| {
